8c283a2891fe5a2de9c6d1d072eaa96115342b40
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-09-07 22:14:19 +0200

    using the .pluck to satisfy uniqueness validation in seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,31 +12,28 @@ url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 cocktail_serialized = open(url).read
 cocktails = JSON.parse(cocktail_serialized)
 
-cocktails.each do |cocktail|
-  #seed ingredients
+cocktails['drinks'].each do |cocktail|
+  # seed ingredients
+  p cocktail
   n = 1
-  p array_cocktails = cocktail['drinks']
-  array_cocktails.each do |thingy|
-    15.times do
-      unless thingy["strIngredient#{n}"].nil?
-        ingredient = Ingredient.new(name: thingy["strIngredient#{n}"])
-        ingredient.save
-        p ingredient
-      end
-        n += 1
-
-  end
+  15.times do
+    unless cocktail["strIngredient#{n}"].nil? && (Ingredient.pluck(:name).include? cocktail["strIngredient#{n}"])
+      created_ingredient = Ingredient.new(name: cocktail["strIngredient#{n}"])
+      created_ingredient.save!
+    end
+    # seed cocktails
+    created_cocktail = Cocktail.create!(name: cocktail['strDrink'], description: cocktail['strInstructions'])
+    # seed doses
+    created_dose = Dose.create!(
+      cocktail: created_cocktail,
+      description: cocktail["strMeasure#{n}"],
+      ingredient: created_ingredient)
+    n += 1
+    p created_dose
   end
 end
 
-    # #seed cocktails
-    # cocktail = Cocktail.create!(name: thingy['strDrink'], description: thingy['strInstructions'])
-    # #seed doses
-    # Dose.create!(
-    #   cocktail: Cocktail.find(n),
-    #   description: thingy["strMeasure#{n}"],
-    #   ingredient: ingredient)
     # #Seed images
-    # file = URI.open(thingy['strDrinkThumb'])
-    # cocktail.photo.attach(io: file, filename: "#{thingy['strDrink'].gsub(" ", "")}.#{thingy['strDrinkThumb'][0...-3]}", content_type: 'image/png')
+    # file = URI.open(cocktail['strDrinkThumb'])
+    # cocktail.photo.attach(io: file, filename: "#{cocktail['strDrink'].gsub(" ", "")}.#{cocktail['strDrinkThumb'][0...-3]}", content_type: 'image/png')
     # p cocktail
