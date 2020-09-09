2d10fa8445c1f3885b000d052175bdc961081d80
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-09-09 13:13:07 +0200

    Work on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,25 @@
 require 'json'
 require 'open-uri'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 cocktail_serialized = open(url).read
 cocktails = JSON.parse(cocktail_serialized)
 
+n = 1
 cocktails['drinks'].each do |cocktail|
   # seed ingredients
-  p cocktail
-  n = 1
-  15.times do
-    if !Ingredient.pluck(:name).include? cocktail["strIngredient#{n}"]
-      Ingredient.create!(name: cocktail["strIngredient#{n}"])
-
-    end
-    # seed cocktails
-    Cocktail.create!(name: cocktail['strDrink'], description: cocktail['strInstructions'])
-    # seed doses
-    Dose.create!(
-      cocktail: Cocktail.find(n),
-      description: cocktail["strMeasure#{n}"],
-      ingredient: Ingredient.find(n))
-    n += 1
+  Cocktail.create!(name: cocktail['strDrink'], description: cocktail['strInstructions'])
+  ingredient_names = Ingredient.pluck(:name)
+  if ingredient_names.include? cocktail["strIngredient#{n}"]
+    Ingredient.create!(name: cocktail["strIngredient#{n}"])
   end
+  # seed cocktails
+  # seed doses
+  Dose.create!(
+    cocktail: Cocktail.find(n),
+    description: cocktail["strMeasure#{n}"],
+    ingredient: Ingredient.find(n))
+  n += 1
 end
 
     # #Seed images
