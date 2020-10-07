d77add94a7b504c024da9fc2cb1a1831e323a15e
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-10-07 12:42:45 +0200

    Search works with PG search and it looks for names and ingredients

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,10 @@
 class CocktailsController < ApplicationController
   def index
-    @cocktails = Cocktail.order(name: :asc)
+    if params[:query].present?
+      @cocktails = Cocktail.global_search(params[:query])
+    else
+      @cocktails = Cocktail.all
+    end
   end
 
   def show
