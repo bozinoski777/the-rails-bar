158e98e10a5e07a21dd4f6990b0351e274ee3675
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-10-15 17:47:38 +0200

    edit modal for adding and deleting an ingretient done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -35,6 +35,11 @@ class CocktailsController < ApplicationController
   @cocktail.update(cocktail_params)
   redirect_to cocktail_path(@cocktail)
   end
+  def destroy
+  @cocktail = Cocktail.find(params[:id])
+  @cocktail.destroy
+  redirect_to cocktail_path(@cocktail)
+  end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
