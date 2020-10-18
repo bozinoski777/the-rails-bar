90a6229e27eb9828881378f5d00c01294c849ae6
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-10-18 16:18:55 +0200

    creating a cocktail test

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -50,14 +50,14 @@ class CocktailsController < ApplicationController
       render :new
     end
 
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @rating = Rating.new(rating_params)
-    @rating.cocktail = @cocktail
-    if @rating.save
-      redirect_to cocktail_path(@cocktail)
-    else
-      render :new
-    end
+    # @cocktail = Cocktail.find(params[:cocktail_id])
+    # @rating = Rating.new(rating_params)
+    # @rating.cocktail = @cocktail
+    # if @rating.save
+    #   redirect_to cocktail_path(@cocktail)
+    # else
+    #   render :new
+    # end
   end
 
   private
