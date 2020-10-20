a57491af6067fd3a00bc42a2273ba8965ef43a3f
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-10-20 13:56:22 +0200

    improved test quality - Homepage (Happy Path)

--- spec/features/homepage_index_cocktail_spec.rb
+++ spec/features/homepage_index_cocktail_spec.rb
@@ -3,33 +3,33 @@ require 'rails_helper'
 RSpec.describe 'Homepage Index' do
 
   before(:context) do
-    Cocktail.find(1).destroy! if Cocktail.where(id: 1).present?
-    Dose.find(1).destroy! if Dose.where(id: 1).present?
-    file = open('/home/hristian/Downloads/400046700953_8407.jpg')
-    Cocktail.create!(name: 'Test Cocktail', id: 1, alcoholic: true).photo.attach(io: file, filename: 'img.png', content_type: 'image/png')
-    Rating.create!(rating: 4, id: 1, cocktail_id: 1)
-    Ingredient.create!(name: 'KUR_DEBEL', id: 1)
-    Dose.create!(description: 'Gin', cocktail_id: 1, ingredient_id: 1)
+    # Cocktail.find(1).destroy! if Cocktail.where(id: 1).present?
+    # Dose.find(1).destroy! if Dose.where(id: 1).present?
+    # file = open('/home/hristian/Downloads/400046700953_8407.jpg')
+    # Cocktail.create!(name: 'Test Cocktail', id: 1, alcoholic: true).photo.attach(io: file, filename: 'img.png', content_type: 'image/png')
+    # Rating.create!(rating: 4, id: 1, cocktail_id: 1)
+    # Ingredient.create!(name: 'KUR_DEBEL', id: 1)
+    # Dose.create!(description: 'Gin', cocktail_id: 1, ingredient_id: 1)
   end
 
   it 'shows the cocktail card' do
     visit('/')
-    find('div.card')
+    find('.card')
   end
 
   it 'shows the cocktail name on card' do
     visit('/')
-    find('div.card_name')
+    find('.card_name')
   end
 
   it 'shows the cocktail rating on card' do
     visit('/')
-    find('div.card_rating')
+    find('.card_rating')
   end
 
   it 'shows the cocktail ingredients on card' do
     visit('/')
-    find('div.card_ingredient')
+    find('.card_ingredient')
   end
 
   it 'navigates to list view'
