cfb292f119420da49ff876fb7e49ea4f4a873b83
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-10-20 14:08:13 +0200

    navigates to list view - Homepage (Happy Path)

--- spec/features/homepage_index_cocktail_spec.rb
+++ spec/features/homepage_index_cocktail_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Homepage Index' do
 
   before(:context) do
-    # Cocktail.find(1).destroy! if Cocktail.where(id: 1).present?
-    # Dose.find(1).destroy! if Dose.where(id: 1).present?
-    # file = open('/home/hristian/Downloads/400046700953_8407.jpg')
-    # Cocktail.create!(name: 'Test Cocktail', id: 1, alcoholic: true).photo.attach(io: file, filename: 'img.png', content_type: 'image/png')
-    # Rating.create!(rating: 4, id: 1, cocktail_id: 1)
-    # Ingredient.create!(name: 'KUR_DEBEL', id: 1)
-    Dose.create!(description: 'Gin', cocktail_id: 1, ingredient_id: 1)
+    Cocktail.find(1).destroy if Cocktail.where(id: 1).present?
+    Dose.find(1).destroy if Dose.where(id: 1).present?
+    file = open('/home/hristian/Downloads/400046700953_8407.jpg')
+    Cocktail.create(name: 'Test Cocktail', id: 1, alcoholic: true).photo.attach(io: file, filename: 'img.png', content_type: 'image/png')
+    Rating.create(rating: 4, id: 1, cocktail_id: 1)
+    Ingredient.create(name: 'KUR_DEBEL', id: 1)
+    Dose.create(description: 'Gin', cocktail_id: 1, ingredient_id: 1)
   end
 
   it 'shows the cocktail card' do
@@ -32,17 +32,30 @@ RSpec.describe 'Homepage Index' do
     find('.card_ingredient')
   end
 
-  it 'navigates to list view'
+  it 'navigates to list view' do
+    visit('/')
+    # find("input[value='list']").click
+    click_button('list')
+    expect(page).to have_content('LISTICLE')
+  end
 
-  it 'shows the cocktail name on list item'
+  it 'shows the cocktail name on list item' do
+    visit('/')
+  end
 
-  it 'shows the cocktail rating on list item'
+  it 'shows the cocktail rating on list item' do
+    visit('/')
+  end
 
-  it 'shows the cocktail ingredients on list item'
+  it 'shows the cocktail ingredients on list item' do
+    visit('/')
+  end
 
-  it 'shows the cocktail alcoholic/virgin on list item'
+  it 'shows the cocktail alcoholic/virgin on list item' do
+    visit('/')
+  end
 
-  it 'shows the cocktail glass type on list item'
-
-
+  it 'shows the cocktail glass type on list item' do
+    visit('/')
+  end
 end
